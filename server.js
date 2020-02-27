21a9ffd9deeb35ab4d266948271a42155c66b545
dorjepadma <dorjepadma@gmail.com>
2020-02-27 15:44:38 -0800

    setup dbj

---
+++ server.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+// Application Dependencies
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+// Database Client
+const client = require('./lib/client');
+const request = require('superagent');
+// Services
+
+// Application Setup
+const app = express();
+app.use(morgan('dev')); // http logging
+app.use(cors()); // enable CORS request
+app.use(express.static('public')); // server files from /public folder
+app.use(express.json()); // enable reading incoming json data
+
+// Auth
+const ensureAuth = require('./lib/auth/ensure-auth');
+const createAuthRoutes = require('./lib/auth/create-auth-routes');
+const authRoutes = createAuthRoutes({
+    async selectUser(email) {
+        const result = await client.query(`
+            SELECT id, email, hash, display_name as "displayName" 
+            FROM users
+            WHERE email = $1;
+        `, [email]);
+        return result.rows[0];
+    },
+    async insertUser(user, hash) {
+        console.log(user);
+        const result = await client.query(`
+            INSERT into users (email, hash, display_name)
+            VALUES ($1, $2, $3)
+            RETURNING id, email, display_name as "displayName";
+        `, [user.email, hash, user.displayName]);
+        return result.rows[0];
+    }
+});
+
+// setup authentication routes
+app.use('/api/auth', authRoutes);
+
+// everything that starts with "/api" below here requires an auth token!
+app.use('/api', ensureAuth);
+
+// *** API Routes ***
+app.get('/api/swapi', async (req, res) => {
+    const data = await request.get(`https://swapi.co/api/people/?search=${req.query.search}`);
+
+    res.json(data.body);
+});
+app.listen(process.env.PORT, () => {
+    console.log('listening at ', process.env.PORT);
+});
